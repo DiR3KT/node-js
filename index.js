48510def5fc67a77a63682f4fc199b985cf4e90e
DiR3KT <clementbahuaud@hotmail.fr>
2019-06-09 15:43:13 +0200

    updated corb errors

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var request = require("request");
+var cors = require('cors');
 var app = express();
+
 var port = process.env.PORT || 8080;
 var api_key = "RGAPI-92178538-42de-44e4-a606-2beffec43318"
 
-app.use(require("cors"))
+app.use(cors());
+
 
 app.get('/', function(req, res){
   res.send("oui");
@@ -12,15 +15,13 @@ app.get('/', function(req, res){
 
 app.get('/championRotation', function(req, res){
   request('https://euw1.api.riotgames.com/lol/platform/v3/champion-rotations?api_key='+api_key, function (error, response, body) {
-    res.send(body);
+    res.jsonp(body);
   })
 })
 
 var name;
 
 app.get('/getData/:region/:name', function(req, res){
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     var region = req.params.region;
     var name = req.params.name;
     if (region == "euw") {
@@ -28,7 +29,7 @@ app.get('/getData/:region/:name', function(req, res){
     }
     URL = "https://"+region+".api.riotgames.com/lol/summoner/v4/summoners/by-name/"+name+"?api_key=";
     request(URL+api_key, function (error, response, body) {
-      res.send(body);
+      res.jsonp(body);
     })
 
 })
